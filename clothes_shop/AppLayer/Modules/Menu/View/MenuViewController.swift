02ec072bf0175907219f706ff31d6728825f5627
t-anastasiia <nastaytalmazan@gmail.com>
2025-03-05 23:05:29 +1000

    Initial commit

---
+++ clothes_shop/AppLayer/Modules/Menu/View/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  clothes_shop
+//
+//  Created by anastasiia talmazan on 2025-03-05.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
